5ac2388a5db5922f88e6e74b00d785d3f2106428
tianyuduan <tianyuduan1991@gmail.com>
2020-03-03 19:48:37 -0800

    bst

--- problemset3.js
+++ problemset3.js
@@ -61,3 +61,37 @@ function levenshteinDistance(str1, str2) {
   // check for number of alphabet matches 
   // abcdef
   // fedcba
+
+
+  // You are given a BST data structure consisting of BST nodes. Each BST node has an integer value stored in a property called "value" and two children nodes stored in 
+  // properties called "left" and "right," respectively. A node is said to be a BST node if and only if it satisfies the BST property: its value is strictly greater than 
+  // the values of every node to its left; its value is less than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves 
+  // or None (null) values. You are also given a target integer value; write a function that finds the closest value to that target value contained in the BST. Assume that there will only be one closest value.
+  function findClosestValueInBst(tree, target) {
+    // Write your code here.
+    bstHelper(tree, target, Infinity)
+  }
+  
+  function findClosestValueInBst(tree, target) {
+    // Write your code here.
+    return bstHelper(tree, target, Infinity)
+  }
+  
+   function bstHelper(tree, target, lowestRecorded) {
+      // as long as it doesnt hit the end of leaf
+     let currentNode = tree;
+      while (currentNode !== null ) {
+        if (Math.abs(currentNode.value - target) < Math.abs(target - lowestRecorded)) {
+          // store lowestRecorded if the current node has a smaller difference then lowestRecorded 
+          lowestRecorded = currentNode.value;
+        }
+        if (currentNode.value > target) {
+          currentNode = currentNode.left
+        } else if (currentNode.value < target) {
+           currentNode = currentNode.right
+        } else {
+          break;
+        }
+        }
+        return lowestRecorded;
+    }
